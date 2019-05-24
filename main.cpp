1f3b14eebb601d39f0ea382a618b77e22ed9fa1f
Josh Palmer <jpalmerwatkins@gmail.com>
2019-05-24 16:59:45 +0100

    Setup QML backend in main.cpp

--- main.cpp
+++ main.cpp
@@ -10,10 +10,22 @@ int main(int argc, char *argv[])
 {
 	Q_INIT_RESOURCE(graphics);
 
-	QGuiApplication::setAttribute(Qt::AA_UseOpenGLES);
 	QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 	QQuickStyle::setStyle("Universal");
-	QQuickWindow::setDefaultAlphaBuffer(true);
+
+	// Setup QML renderer
+	QString productType = QSysInfo::productType();
+	QString productVersion = QSysInfo::productVersion();
+
+	if(productType == "windows" && productVersion == "10")
+	{
+		QQuickWindow::setSceneGraphBackend(QSGRendererInterface::Direct3D12);
+	}
+	else
+	{
+		// Force OpenGL ES to prevent crashes on windows
+		QGuiApplication::setAttribute(Qt::AA_UseOpenGLES);
+	}
 
 	qRegisterMetaType<HWND>();
 	qRegisterMetaType<QScreen*>();
